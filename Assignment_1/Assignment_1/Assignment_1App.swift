dea0c83f2d830d9d68859923a5ec3bbd7b457d27
taehyunL <taylor1233@naver.com>
2021-11-17 17:03:48 +0900

    initial commit

---
+++ Assignment_1/Assignment_1/Assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_1App.swift
+//  Assignment_1
+//
+//  Created by 이태현 on 2021/11/17.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
