953f10527108dd6e972a76c6ad379e45e33b72dc
taehyunL <taylor1233@naver.com>
2021-11-22 22:37:20 +0900

    Model

---
+++ Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,55 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by 이태현 on 2021/11/19.
+//
+
+//Model
+import Foundation
+
+struct MemoryGame<CardContent> where CardContent: Equatable {
+    private(set) var cards: Array<Card>
+    
+    private var indexOfTheOneOnlyFaceUpCard: Int?
+    
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id}),
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched
+        {
+            if let potentialMatchIndex = indexOfTheOneOnlyFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+//                    cards.remove(at: chosenIndex)
+//                    cards.remove(at: potentialMatchIndex)
+                }
+                indexOfTheOneOnlyFaceUpCard = nil
+            } else {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheOneOnlyFaceUpCard = chosenIndex
+            }
+            cards[chosenIndex].isFaceUp.toggle()
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createCardContent(pairIndex)
+            cards.append(Card(content: content,id: pairIndex*2))
+            cards.append(Card(content: content,id: pairIndex*2+1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
