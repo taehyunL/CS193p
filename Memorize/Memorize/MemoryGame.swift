aabaec117c2566bcf9edaa52a2352b03450739b4
taehyunL <taylor1233@naver.com>
2022-01-01 22:06:06 +0900

    앞면 보이게

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -44,7 +44,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = false
+        var isFaceUp: Bool = true
         var isMatched: Bool = false
         let content: CardContent
         let id: Int
