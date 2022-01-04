4233a83a524092a2216d32256d856568f7190c6a
taehyunL <taylor1233@naver.com>
2022-01-05 00:59:28 +0900

    model설정 choose에서 변경점 남아있음

--- Assignment_3/Assignment_3/Model/SetGame.swift
+++ Assignment_3/Assignment_3/Model/SetGame.swift
@@ -7,19 +7,28 @@
 
 import Foundation
 
-struct SetGame {
+struct SetGame<CardContent> where CardContent: Equatable {
     private(set) var cards: [Card]
+    private(set) var cnt = 0
     
-    init(numberOfCards: Int) {
+    init(numberOfCards: Int, createCardcontent: (Int) -> CardContent) {
         cards = []
         
-        for pairOfCards in 0..<numberOfCards {
-            cards.append(Card(id: pairOfCards))
+        for pairIndex in 0..<numberOfCards {
+            let content = createCardcontent(pairIndex)
+            cards.append(Card(content: content, id: pairIndex))
         }
+    }
+    
+    mutating func choose(_ card: Card) {
+        cards[card.id].isChoosen = !cards[card.id].isChoosen
+        cards.remove(at: card.id)
     }
     
     struct Card: Identifiable {
         var isChoosen: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
         var id: Int
     }
 }
