009a8d172da2b9800763dbbbc7682839a65fd708
taehyunL <taylor1233@naver.com>
2022-01-04 17:02:38 +0900

    초기 view 설정

--- Assignment_3/Assignment_3/View/ContentView.swift
+++ Assignment_3/Assignment_3/View/ContentView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var game: SetGameViewModel
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
+                ForEach(game.cards) { card in
+                    CardView(card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                }
+            }
+        }
     }
 }
 
-
-
+struct CardView: View {
+    var card: SetGame.Card
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 10)
+            shape.fill().foregroundColor(.white)
+            shape.strokeBorder(lineWidth: 3)
+//            Text(card.content) 여기에 카드 content 추가
+//            shape.opacity(0) 투명도 설정하기 
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let game = SetGameViewModel()
+        
+        ContentView(game: game)
     }
 }
