c1cd5f39626e4022eaccae168b0a52e5250d5840
taehyunL <taylor1233@naver.com>
2022-01-04 15:41:24 +0900

    initial View

---
+++ Assignment_3/Assignment_3/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment_3
+//
+//  Created by 이태현 on 2022/01/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
