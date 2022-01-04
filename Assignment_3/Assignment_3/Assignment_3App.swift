f57de59c5c8eefcd877140502b49ec62086304a3
taehyunL <taylor1233@naver.com>
2022-01-04 15:41:13 +0900

    initial commit

---
+++ Assignment_3/Assignment_3/Assignment_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_3App.swift
+//  Assignment_3
+//
+//  Created by 이태현 on 2022/01/02.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
