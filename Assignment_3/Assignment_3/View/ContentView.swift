47a24a1b52e340a4131e2d22e80fbcdeb7679e86
taehyunL <taylor1233@naver.com>
2022-01-05 01:00:05 +0900

    emoji가 아닌 Rhombus 활용형태로 바꿔야함

--- Assignment_3/Assignment_3/View/ContentView.swift
+++ Assignment_3/Assignment_3/View/ContentView.swift
@@ -11,28 +11,61 @@ struct ContentView: View {
     @ObservedObject var game: SetGameViewModel
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
-                ForEach(game.cards) { card in
-                    CardView(card: card)
-                        .aspectRatio(2/3, contentMode: .fit)
-                }
+        AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
+            if card.isChoosen == true && card.isMatched == false {
+                CardView(card: card)
+                    .foregroundColor(.blue)
+                    .padding(4)
+                    .onTapGesture {
+                        game.choose(card)
+                        print("check true")
+                    }
+            } else if card.isChoosen == false && card.isMatched == false {
+                CardView(card: card)
+                    .foregroundColor(.red)
+                    .padding(4)
+                    .onTapGesture {
+                        game.choose(card)
+                        print("check false")
+                    }
+            }
+        }
+        .foregroundColor(.red)
+        .padding(.horizontal)
+    }
+}
+
+struct CardView: View {
+    var card: SetGame<String>.Card
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: 10)
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(card.content)
+//                Rhombus(geometry: geometry)
+    //            Text(card.content) 여기에 카드 content 추가
+    //            shape.opacity(0) 투명도 설정하기
+
             }
         }
     }
 }
 
-struct CardView: View {
-    var card: SetGame.Card
+struct Rhombus: View {
+    var geometry: GeometryProxy
     
     var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 10)
-            shape.fill().foregroundColor(.white)
-            shape.strokeBorder(lineWidth: 3)
-//            Text(card.content) 여기에 카드 content 추가
-//            shape.opacity(0) 투명도 설정하기 
+        Path { path in
+            path.move(to: CGPoint(x: geometry.size.width / 2, y: 0))
+            path.addLine(to: CGPoint(x: geometry.size.width, y: geometry.size.height / 2))
+            path.addLine(to: CGPoint(x: geometry.size.width / 2, y: geometry.size.height))
+            path.addLine(to: CGPoint(x: 0, y: geometry.size.height / 2))
+            path.addLine(to: CGPoint(x: geometry.size.width / 2, y: 0))
         }
+        .fill(Color.white)
     }
 }
 
