aed10d5fe74db4c46039e2638809be62bb803f14
taehyunL <taylor1233@naver.com>
2022-01-05 17:35:22 +0900

    set카드 3장 찾을시 제거 아니면 원상복귀시키기

--- Assignment_3/Assignment_3/Model/SetGame.swift
+++ Assignment_3/Assignment_3/Model/SetGame.swift
@@ -9,10 +9,16 @@ import Foundation
 
 struct SetGame<CardContent> where CardContent: Equatable {
     private(set) var cards: [Card]
-    private(set) var cnt = 0
+    private var chooseNumber: Int
+    private var cardsIndex1: Int?
+    private var cardsIndex2: Int?
+    private var cardsIndex3: Int?
+    private var cardsIndex: [Int]
     
     init(numberOfCards: Int, createCardcontent: (Int) -> CardContent) {
         cards = []
+        chooseNumber = 0
+        cardsIndex = []
         
         for pairIndex in 0..<numberOfCards {
             let content = createCardcontent(pairIndex)
@@ -21,8 +27,46 @@ struct SetGame<CardContent> where CardContent: Equatable {
     }
     
     mutating func choose(_ card: Card) {
-        cards[card.id].isChoosen = !cards[card.id].isChoosen
-        cards.remove(at: card.id)
+        cards[card.id].isChoosen.toggle()
+        if card.isChoosen == false {
+            chooseNumber += 1
+        } else {
+            chooseNumber -= 1
+        }
+        if chooseNumber == 1 {
+            cardsIndex1 = card.id
+            cardsIndex.append(cardsIndex1!)
+        }
+        
+        if chooseNumber == 2 {
+            cardsIndex2 = card.id
+            cardsIndex.append(cardsIndex2!)
+        }
+        
+        if chooseNumber == 3 {
+            cardsIndex3 = card.id
+            cardsIndex.append(cardsIndex3!)
+            
+            if cards[cardsIndex1!].content == cards[cardsIndex2!].content && cards[cardsIndex2!].content == cards[cardsIndex3!].content {
+                let sort = cardsIndex.sorted(by: > )
+                cards.remove(at: sort[0])
+                cards.remove(at: sort[1])
+                cards.remove(at: sort[2])
+                
+                for index in cards.indices {
+                    cards[index].id = index
+                }
+            } else {
+                for index in cards.indices {
+                    if cards[index].isChoosen == true {
+                        cards[index].isChoosen.toggle()
+                    }
+                }
+            }
+            chooseNumber = 0
+            cardsIndex = []
+        }
+        
     }
     
     struct Card: Identifiable {
